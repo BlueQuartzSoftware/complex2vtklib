03cc9540a56681009a11d3bd6ef951cb70bfd05f
Michael Jackson <mike.jackson@bluequartz.net>
2021-12-07 11:45:17 -0500

    API Update changes
    Signed-off-by: Michael Jackson <mike.jackson@bluequartz.net>

--- src/example/Utilities.hpp
+++ src/example/Utilities.hpp
@@ -54,10 +54,9 @@ void ImportStlFile(std::shared_ptr<DataStructure>& dataStructure)
 
     // Create default Parameters for the filter.
     args.insertOrAssign(StlFileReaderFilter::k_StlFilePath_Key, std::make_any<FileSystemPathParameter::ValueType>(fs::path(inputFile)));
-    args.insertOrAssign(StlFileReaderFilter::k_ParentDataGroupPath_Key, std::make_any<DataPath>(parentPath));
-    args.insertOrAssign(StlFileReaderFilter::k_GeometryName_Key, std::make_any<std::string>(triangleGeometryName));
-    args.insertOrAssign(StlFileReaderFilter::k_FaceDataGroupName_Key, std::make_any<std::string>(triangleFaceDataGroupName));
-    args.insertOrAssign(StlFileReaderFilter::k_FaceNormalsArrayName_Key, std::make_any<DataPath>(normalsDataPath));
+    args.insertOrAssign(StlFileReaderFilter::k_GeometryDataPath_Key, std::make_any<DataPath>({triangleGeometryName}));
+    args.insertOrAssign(StlFileReaderFilter::k_FaceGroupDataPath_Key, std::make_any<DataPath>({triangleFaceDataGroupName}));
+    args.insertOrAssign(StlFileReaderFilter::k_FaceNormalsDataPath_Key, std::make_any<DataPath>(normalsDataPath));
 
     // Preflight the filter and check result
     auto preflightResult = filter.preflight(dataGraph, args);
